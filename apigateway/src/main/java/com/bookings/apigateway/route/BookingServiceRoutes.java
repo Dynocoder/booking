7aee6bc72bcc2003a8bd19f780d5f1857f0edd5a
Dynocoder <sauravprashar21@gmail.com>
2026-04-05 18:47:04 -0400

    feature: implemented api gateway to route traffic to booking and inventory services

---
+++ apigateway/src/main/java/com/bookings/apigateway/route/BookingServiceRoutes.java
@@ -0,0 +1,25 @@
+package com.bookings.apigateway.route;
+
+import org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+/**
+ * BookingServiceRoutes
+ */
+@Configuration
+public class BookingServiceRoutes {
+
+	@Bean
+	public RouterFunction<ServerResponse> bookingRoutes() {
+		return GatewayRouterFunctions.route("booking-service")
+				.route(RequestPredicates.path("/api/v1/bookings"), HandlerFunctions.http())
+				.before(BeforeFilterFunctions.uri("http://localhost:8081"))
+				.build();
+	}
+}
---
+++ apigateway/src/main/java/com/bookings/apigateway/route/InventoryServiceRoutes.java
@@ -0,0 +1,27 @@
+
+package com.bookings.apigateway.route;
+
+import org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+/**
+ * InventoryServiceRoutes
+ */
+@Configuration
+public class InventoryServiceRoutes {
+
+	@Bean
+	public RouterFunction<ServerResponse> inventoryRoutes() {
+		return GatewayRouterFunctions.route("inventory-service")
+				.route(RequestPredicates.path("/api/v1/inventory/**"), HandlerFunctions.http())
+				.before(BeforeFilterFunctions.uri("http://localhost:8080"))
+				.build();
+	}
+
+}
